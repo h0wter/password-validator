09b9fd0f61ce8d15ab4425fd37f914293d73cc7a
h0wter <howter99@gmail.com>
2023-06-21 05:11:21 +0300

    add: password validation for 5 indicators

--- src/app/services/password-validator.service.ts
+++ src/app/services/password-validator.service.ts
@@ -5,21 +5,75 @@ import { CharTypes, PasswordStrengthType } from '../types/password-types';
   providedIn: 'root',
 })
 export class PasswordValidatorService {
-  validate(value: string): PasswordStrengthType {
+  validate(value: string, indicatorsCount: number = 3): PasswordStrengthType {
     const password = value.trim();
     const typesCount = this.typesCount(password);
 
-    if (password === '') {
-      return 'empty';
-    } else if (password.length < 8) {
-      return 'short';
-    } else if (typesCount === 1) {
-      return 'easy';
-    } else if (typesCount === 2) {
-      return 'medium';
-    } else {
-      return 'strong';
+    switch (indicatorsCount) {
+      case 3:
+        switch (typesCount) {
+          case 1:
+            if (password.length < 8) {
+              return 'short';
+            } else {
+              return 'easy';
+            }
+          case 2:
+            if (password.length < 8) {
+              return 'short';
+            } else {
+              return 'medium';
+            }
+          case 3:
+            if (password.length < 8) {
+              return 'short';
+            } else {
+              return 'strong';
+            }
+          default:
+            return 'empty';
+        }
+      case 5:
+        switch (typesCount) {
+          case 1:
+            if (password.length < 6) {
+              return 'veryShort';
+            } else if (password.length < 8) {
+              return 'short';
+            } else if (password.length < 10) {
+              return 'easy';
+            } else {
+              return 'medium';
+            }
+          case 2:
+            if (password.length < 6) {
+              return 'short';
+            } else if (password.length < 8) {
+              return 'easy';
+            } else if (password.length < 10) {
+              return 'medium';
+            } else if (password.length < 12) {
+              return 'strong';
+            } else {
+              return 'veryStrong';
+            }
+          case 3:
+            if (password.length < 6) {
+              return 'easy';
+            } else if (password.length < 8) {
+              return 'medium';
+            } else if (password.length < 10) {
+              return 'strong';
+            } else if (password.length < 12) {
+              return 'veryStrong';
+            } else {
+              return 'extremelyStrong';
+            }
+          default:
+            return 'empty';
+        }
     }
+    return 'empty';
   }
 
   private typesCount(password: string): number {
@@ -44,6 +98,6 @@ export class PasswordValidatorService {
   }
 
   private isLetter(char: string): boolean {
-    return /[a-zA-Z]/.test(char);
+    return /[a-zA-Zа-яА-Я]/.test(char);
   }
 }
