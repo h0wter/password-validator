2dda88cbef033dad4d4f3c9a755d70a8d74aae9e
h0wter <howter99@gmail.com>
2023-06-21 05:55:56 +0300

    update: formatted with prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PasswordStrengthComponent } from './components/password-strength/password-strength.component';
+import { PasswordInputComponent } from './components/password-input/password-input.component';
 
 @NgModule({
-  declarations: [AppComponent, PasswordStrengthComponent],
+  declarations: [
+    AppComponent,
+    PasswordStrengthComponent,
+    PasswordInputComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
