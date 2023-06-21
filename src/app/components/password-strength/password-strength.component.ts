9d06a4600a736fe61cc5afbb54cebb589e1ad1f1
h0wter <howter99@gmail.com>
2023-06-21 05:12:31 +0300

    add: ability to dynamically change the number of indicators

--- src/app/components/password-strength/password-strength.component.ts
+++ src/app/components/password-strength/password-strength.component.ts
@@ -1,32 +1,37 @@
-import { Component, inject } from '@angular/core';
-import { passwordStrengthConfig } from './password-strength.config';
+import { Component, Input, inject } from '@angular/core';
 import { PasswordValidatorService } from 'src/app/services/password-validator.service';
-import {
-  PasswordStrengthType,
-  PositionType,
-} from 'src/app/types/password-types';
+import { PasswordStrengthType } from 'src/app/types/password-types';
+import { IndicatorColorsService } from 'src/app/services/indicator-colors.service';
 
 @Component({
   selector: 'app-password-strength',
   templateUrl: './password-strength.component.html',
 })
 export class PasswordStrengthComponent {
-  private _password = '';
+  private labelsCount = 3;
   private _passwordService = inject(PasswordValidatorService);
+  private _password = '';
   private _passwordStrength: PasswordStrengthType = 'empty';
-  labels: PositionType[] = ['left', 'middle', 'right'];
+  colorsService = inject(IndicatorColorsService);
+  labelsIndexArray = Array.from(
+    { length: this.labelsCount },
+    (_, index) => index
+  );
   isVisible = false;
 
   set password(value: string) {
     this._password = value;
-    this._passwordStrength = this._passwordService.validate(this._password);
+    this._passwordStrength = this._passwordService.validate(
+      this._password,
+      this.labelsIndexArray.length
+    );
   }
 
   togglePasswordVisible(): void {
     this.isVisible = !this.isVisible;
   }
 
-  getColorClass(position: 'left' | 'middle' | 'right'): string {
-    return passwordStrengthConfig[position][this._passwordStrength];
+  getColorClass() {
+    return this.colorsService.getColorsArray(this._passwordStrength);
   }
 }
